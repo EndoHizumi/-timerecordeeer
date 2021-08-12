cde7f3571154e6a57863d589dbd6407173c90b43
Endo_Hizumi <endohizumi0114@gmail.com>
2021-08-13 00:27:39 +0900

    wip: emboss_handlerで設定ファイルを読み込む

--- time_recordeeer/emboss_handler.py
+++ time_recordeeer/emboss_handler.py
@@ -2,6 +2,7 @@ from time_recordeeer.freee import freee
 
 
 def handle(args):
+    # 設定ファイルを読み込む
     # company_idとemp_idを取得する。
     api = freee()
     # タイムレコーダー登録APIを叩く
--- time_recordeeer/freee.py
+++ time_recordeeer/freee.py
@@ -11,30 +11,42 @@ class freee:
     company_id = ''
     emp_id = ''
 
-    def __init__(self):
-        script_path = os.path.dirname(os.path.abspath(__file__))
-        with open(f'{script_path}/config.json') as f:
-            config = json.load(ｆ)
-            if config.get('access_token'):
-                self.headers['Authorization'] = f'Bearer {config.get("access_token")}'
-                emp_info = requests.get("https://api.freee.co.jp/hr/api/v1/users/me", headers=self.headers).json()
-                self.company_id = emp_info['companies'][0]['id']
-                self.emp_id = emp_info['companies'][0]['employee_id']
-            else:
-                print('config.jsonにアクセストークンが登録されていません。認証ページにログインしてアクセストークンを取得してください')
-                token_url = f"{config['token_url']}?client_id={config['client_id']}&redirect_uri={config['redirect_uri']}&response_type={config['response_type']}"
-                webbrowser.open(token_url)
-                return
+    def __init__(self, config_data: dict):
+        config = config_data
+        if not config.get('access_token'):
+            print('config.jsonにアクセストークンが登録されていません。認証ページにログインして認可コードを取得してください')
+            token_url = f'https://accounts.secure.freee.co.jp/public_api/authorize?client_id={config["client_id"]}&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&response_type=code'
+            webbrowser.open(token_url)
+            auth_code: str = input()
+            self.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+            body = {
+                'grant_type': 'authorization_code',
+                'client_id': config['client_id'],
+                'client_secret': config['client_secret'],
+                'code': auth_code,
+                'redirect_uri': 'urn:ietf:wg:oauth:2.0:oob'
+            }
+            res = requests.post('https://accounts.secure.freee.co.jp/public_api/token', headers=self.headers, data=body).json()
+            config['access_token'] = res['access_token']
+            config['refresh_token'] = res['refresh_token']
+        
+        self.headers['Authorization'] = f'Bearer {config.get("access_token")}'
+        emp_info = requests.get('https://api.freee.co.jp/hr/api/v1/users/me', headers=self.headers).json()
+        self.company_id = emp_info['companies'][0]['id']
+        self.emp_id = emp_info['companies'][0]['employee_id']
+        return config
 
-    def register_time_clocks(self, state: str):
-        payload: Dict[str, str] = {'company_id': self.company_id, 'type': state, 'emp_id': self.emp_id, 'base_date': date.today().strftime('%Y-%m-%d')}
-        responce = requests.post(f"https://api.freee.co.jp/hr/api/v1/employees/{emp_id}/time_clocks", headers=self.headers, data=payload)
+    def register_time_clocks(self, たstate: str):
+        payload: Dict[str, str] = {'company_id': self.company_id, 'type': state,
+                                   'emp_id': self.emp_id, 'base_date': date.today().strftime('%Y-%m-%d')}
+        responce = requests.post(
+            f'https://api.freee.co.jp/hr/api/v1/employees/{emp_id}/time_clocks', headers=self.headers, data=payload)
         if not responce.status_code == 201:
             raise ValueError(responce.json())
         return responce.json()
 
     def get_available_type(self) -> str:
         attendance_state_map = {'break_begin': 'clockIn', 'break_out': 'breakIn', 'clock_in': 'clockOut'}
-        url = f"https://api.freee.co.jp/hr/api/v1/employees/{self.emp_id}/time_clocks/available_types?company_id={self.company_id}"
+        url = f'https://api.freee.co.jp/hr/api/v1/employees/{self.emp_id}/time_clocks/available_types?company_id={self.company_id}'
         responce = requests.get(url, headers=self.headers).json()
         return attendance_state_map[responce['available_types'][0]]
