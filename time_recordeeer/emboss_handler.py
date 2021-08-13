54429e509d92b66e53c1ef986a9148a9e5c1c209
Shunsuke Kikuchi <t-shunkikuch@yahoo-corp.jp>
2021-08-13 18:07:08 +0900

    wip

--- time_recordeeer/emboss_handler.py
+++ time_recordeeer/emboss_handler.py
@@ -1,11 +1,15 @@
 from time_recordeeer.freee import freee
 import os
+import json
 
 def handle(args):
     # 設定ファイルを読み込む
-    # company_idとemp_idを取得する。
     config_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-    api = freee(config_path)
+    with open(os.path.join(config_path, 'config.json')) as f:
+        # company_idとemp_idを取得する。
+        api = freee(json.load(f))
     # タイムレコーダー登録APIを叩く
-    res = api.register_time_clocks(args.state)
-    return res
+    if not api.get_message():
+        res = api.register_time_clocks(args.state)
+        return res
+    return api.get_message()
--- time_recordeeer/freee.py
+++ time_recordeeer/freee.py
@@ -4,13 +4,16 @@ import webbrowser
 from datetime import date
 from typing import Dict
 
+from requests.models import Response
+
 
 class freee:
     headers = {'Authorization': '', 'accept': 'application/json'}
     company_id = ''
     emp_id = ''
+    message = ''
 
-    def __init__(self, config_data: dict):
+    def __init__(self, config_data: dict) -> None:
         config = config_data
         if not config.get('access_token'):
             print('config.jsonにアクセストークンが登録されていません。認証ページにログインして認可コードを取得してください')
@@ -25,15 +28,20 @@ class freee:
                 'code': auth_code,
                 'redirect_uri': 'urn:ietf:wg:oauth:2.0:oob'
             }
-            res = requests.post('https://accounts.secure.freee.co.jp/public_api/token', headers=self.headers, data=body).json()
+            res = requests.post(
+                'https://accounts.secure.freee.co.jp/public_api/token', headers=self.headers, data=body).json()
             config['access_token'] = res['access_token']
             config['refresh_token'] = res['refresh_token']
-        
+
         self.headers['Authorization'] = f'Bearer {config.get("access_token")}'
-        emp_info = requests.get('https://api.freee.co.jp/hr/api/v1/users/me', headers=self.headers).json()
-        self.company_id = emp_info['companies'][0]['id']
-        self.emp_id = emp_info['companies'][0]['employee_id']
-        return config
+        res = requests.get(
+            'https://api.freee.co.jp/hr/api/v1/users/me', headers=self.headers)
+        emp_info = res.json()
+        if res.status_code == 200:
+            self.company_id = emp_info['companies'][0]['id']
+            self.emp_id = emp_info['companies'][0]['employee_id']
+        else:
+             self.message = emp_info.get('message')
 
     def register_time_clocks(self, たstate: str):
         payload: Dict[str, str] = {'company_id': self.company_id, 'type': state,
@@ -45,7 +53,11 @@ class freee:
         return responce.json()
 
     def get_available_type(self) -> str:
-        attendance_state_map = {'break_begin': 'clockIn', 'break_out': 'breakIn', 'clock_in': 'clockOut'}
+        attendance_state_map = {'break_begin': 'clockIn',
+                                'break_out': 'breakIn', 'clock_in': 'clockOut'}
         url = f'https://api.freee.co.jp/hr/api/v1/employees/{self.emp_id}/time_clocks/available_types?company_id={self.company_id}'
         responce = requests.get(url, headers=self.headers).json()
         return attendance_state_map[responce['available_types'][0]]
+
+    def get_message(self) -> str:
+        return self.message
